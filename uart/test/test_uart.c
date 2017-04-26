34fda495a7defa5935837d19ac21b91f3a59a7bf
qz <zhaoqige@163.com>
2017-04-26 16:01:05 +0800

    Modified, but not verified on real device

--- uart/test/test_uart.c
+++ uart/test/test_uart.c
@@ -1,41 +1,65 @@
-#include "six_uart.h"
+// written by Zhenqiang Liu
+// modified by Qige Zhao @ 2017.04.26
+
 #include <stdio.h>
-#include <termios.h>
-#include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
+
+#include <termios.h>
+#include <fcntl.h>
+
 #include <signal.h>
 
-int fd=0;
+#include "six_uart.h"
 
-void main_exit()
+// uart file descriptor
+static int fd = 0;
+
+// handler of ^c, ^\, kill
+void main_exit(void)
 {
 	uart_close(&fd);
-	kill(0,SIGQUIT);
+	//kill(0, SIGQUIT);
 }
 
-int main()
+// open device
+// write, read
+// close when exit
+int main(void)
 {
-	int ret=0;
-	char buf[10]={0};
-	fd=uart_open("/dev/ttyUSB0");
-	signal(SIGINT, 	(void *) main_exit);
-	while(1)
+	int ret = 0;
+	const uart_dev = "/dev/ttyUSB0";
+	const char *buf = "ABCDEFG\n";
+	char rbuf[128];
+	const unsigned int idle = 1;
+
+	fd = uart_open(uart_dev);
+	if (fd < 0) {
+		printf("err> open uart failed.\n");
+		return -1;
+	}
+
+	// exit when user wanted
+	signal(SIGINT, 	(void *) main_exit); // ^c
+
+	for(;;)
 	{
-		bzero(buf,10);
-		strcpy(buf,"ABCDEFGHI");
-		ret=uart_write(fd,buf,9);
-		if(ret>0)
-			printf("PC urat_write %d\n",ret);
-		else
+		ret = uart_write(fd, buf, strlen(buf));
+		if(ret > 0) {
+			printf("PC urat_write %d bytes\n",ret);
+		} else {
 			printf("PC write fail\n");
-		bzero(buf,10);
-		sleep(1);
-		ret=uart_read(fd,buf,9);
-		if(ret>0)
-			printf("PC urat_read %s\n",buf);
-		else
+		}
+		sleep(idle);
+
+		memset(rbuf, 0, sizeof(rbuf));
+		ret = uart_read(fd, rbuf, sizeof(rbuf)-1);
+		if(ret > 0) {
+			printf("PC urat_read [%s]\n", rbuf);
+		} else {
 			printf("PC read fail\n");
+		}
+		sleep(idle);
 	}
 	return 0;
 }
